e881505cad3a85bf9c6ca7f81fa01ed3d9c5fb48
kevinqtogitty <kevinq.to@gmail.com>
2022-06-04 11:18:38 +0200

    Created index, added css, added animations and submenu

---
+++ main.js
@@ -0,0 +1,45 @@
+// const home = document.getElementById("home");
+// const faces = document.querySelector("#faces");
+// const landscapes = document.querySelector("#landscapes");
+// const projects = document.querySelector("#projects");
+// const store = document.querySelector("#store");
+// const about = document.querySelector("#about");
+// const instagram = document.querySelector("#instagram");
+
+const projects = document.querySelector('.projects');
+
+const submenuDisappear = document.querySelector('#submenu');
+
+// document.querySelectorAll('.navItem').forEach(item => {
+//     item.addEventListener('mouseover', e => {
+//         item.style.color = 'white';
+//     })
+// })
+
+// document.querySelectorAll('.navItem').forEach(item => {
+//     item.addEventListener('mouseout', e => {
+//         item.style.color = 'black';
+//     })
+// })
+
+
+// function mouseover(item) {
+//     item.style.color = 'white';
+// }
+
+// function mouseout(item) {
+//     item.style.color = 'black';
+// }
+
+
+// projects.addEventListener('mouseover', () => {
+//     submenu.style.display = 'block';
+// })
+
+// // projects.addEventListener('mouseout', () => {
+// //     submenu.style.display = 'none';
+// // })
+
+// submenuDisappear.addEventListener('mouseleave', () => {
+//     submenu.style.display = 'none';
+// })
---
+++ secondary.js
@@ -0,0 +1,13 @@
+
+
+// const projects = document.querySelector('.projects');
+
+// projects.addEventListener('mouseover', e => {
+//     projects[0].style2.color = 'red';
+// })
+
+const projects = document.querySelector('.projects');
+
+projects.addEventListener('mouseover', () => {
+    submenu.style.display = 'block';
+})
